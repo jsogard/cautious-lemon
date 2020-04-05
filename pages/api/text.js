f8eb7c3979d875d53810cfcf68501a94ff4bd4d0
Joe Sogard <jmsogard@gmail.com>
2020-04-05 15:19:14 -0500

    add connections to database

--- pages/api/text.js
+++ pages/api/text.js
@@ -1,5 +1,15 @@
-export default (req, res) => {
+const db = require('../../lib/db');
+const escp = require('sql-template-strings')
+
+
+export default async (req, res) => {
+
+	const test = await db.query(escp`
+		SELECT *
+		FROM test
+		`);
+
 	res.status(200).json({
-		text: "this is a good api"
+		test
 	});
 };
